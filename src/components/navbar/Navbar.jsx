f9be0cb099a7cea11bb656dc0441b086bed9f8d2
IsraelBosun <israelbosun1@gmail.com>
2023-01-03 23:00:50 +0100

    first commital

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { RiMenu3Line, RiCloseLine  } from "react-icons/ri"
+import logo from "../../assets/logo.svg"
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      <div className='gpt3__navbar-links_container'>
+        <p><a href="#">Home</a></p>
+        <p><a href="#wgpt3">What is GPT3</a></p>
+        <p><a href="#possibility">Open AI</a></p>
+        <p><a href="#features">Case Studies</a></p>
+        <p><a href="#blog">Library</a></p>
+      </div>  
+      </div>
+      
+      <div className='gpt__navbar-sign'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
