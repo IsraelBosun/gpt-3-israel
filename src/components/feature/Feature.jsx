68ac75ecf47071a93fd620232a86a1b33afa8e30
IsraelBosun <israelbosun1@gmail.com>
2023-01-05 01:11:57 +0100

    making What GPT3 and feature

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./feature.css"
 
+// const Feature = ({ title, text}) =>
 const Feature = () => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container-feature-extending'>
+        {text}
+      </div>
+    </div>
   )
 }
 
